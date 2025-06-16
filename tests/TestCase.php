25bcf23bfcf13074ff9e9957f39ed5b2aeab4731
onyiimesi <onyedikachukwu62@gmail.com>
2025-06-16 15:11:01 +0100

    chore: remove private repo reference

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,7 @@ abstract class TestCase extends BaseTestCase
     protected function getPackageProviders($app)
     {
         return [
-            \Fixit\FixitServiceProvider::class,
+            \Fixit\FixItServiceProvider::class,
         ];
     }
 
